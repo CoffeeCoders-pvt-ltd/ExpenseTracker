7a09599206e74fb7f262361de9cc74e9c374d3b9
Biraj Mainali <biraj.prathamit@gmail.com>
2021-11-07 14:08:30 +0545

    feat : ensuring workspace is created

--- src/ExpenseTracker.Web/Controllers/HomeController.cs
+++ src/ExpenseTracker.Web/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ExpenseTracker.Common.Model;
+using ExpenseTracker.Core.Entities;
 using ExpenseTracker.Core.Entities.Common;
 using ExpenseTracker.Core.Repositories.Interface;
 using ExpenseTracker.Infrastructure.Extensions;
@@ -22,7 +24,9 @@ namespace ExpenseTracker.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IUserProvider userProvider;
 
-        public HomeController(ITransactionRepository transactionRepository,ITransactionCategoryRepository transactionCategoryRepository,ILogger<HomeController> logger, IUserProvider userProvider)
+        public HomeController(ITransactionRepository transactionRepository,
+            ITransactionCategoryRepository transactionCategoryRepository, ILogger<HomeController> logger,
+            IUserProvider userProvider)
         {
             _transactionRepository = transactionRepository;
             _transactionCategoryRepository = transactionCategoryRepository;
@@ -32,55 +36,67 @@ namespace ExpenseTracker.Web.Controllers
 
         public async Task<IActionResult> Index(HomeViewModel homeViewModel)
         {
+            var workspaces = (await userProvider.GetCurrentUser()).Workspaces;
+            if (!workspaces.Any(x => x.Status == BaseModel.StatusActive))
+            {
+                return Redirect("/Workspace/Create");
+            }
+
             var workspaceToken = (await userProvider.GetCurrentUser()).DefaultWorkspace.Token;
 
-            var transactionQueryable = _transactionRepository.GetPredicatedQueryable(a => a.Workspace.Token == workspaceToken);
-            
-            homeViewModel.Transactions = transactionQueryable.OrderByDescending(a=>a.TransactionDate).Take(5).ToList();
-                homeViewModel.TopExpendingCategories = transactionQueryable
-                    .Where(a => 
+            var transactionQueryable =
+                _transactionRepository.GetPredicatedQueryable(a => a.Workspace.Token == workspaceToken);
+
+            homeViewModel.Transactions =
+                transactionQueryable.OrderByDescending(a => a.TransactionDate).Take(5).ToList();
+            homeViewModel.TopExpendingCategories = transactionQueryable
+                .Where(a =>
                     a.Type == TransactionType.Expense &&
                     a.TransactionDate.Date >= DateTime.Today.AddMonths(-1).Date &&
                     a.TransactionDate.Date <= DateTime.Today.Date)
-                    .GroupBy(a=> new { CategoryName = a.TransactionCategory.CategoryName, CategoryId = a.TransactionCategory.Id, a.TransactionCategory.Color })
-                    .Select(x => new TopCategory()
-                    {
-                        CategoryName = x.Key.CategoryName,
-                        Amount = x.Sum(z=>z.Amount),
-                        CategoryId = x.Key.CategoryId,
-                        Color = x.Key.Color
-                    }).OrderByDescending(a => a.Amount).ToList();
-                homeViewModel.AllCategories = await _transactionCategoryRepository.GetAllAsync().ConfigureAwait(true);
-                homeViewModel.DailyExpenseAmount = transactionQueryable.Where(a =>
-                        a.TransactionDate.Date == DateTime.Today.Date && a.Type == TransactionType.Expense).ToList()
-                    .Sum(a => a.Amount);
-                homeViewModel.DailyIncomeAmount = transactionQueryable.Where(a =>
-                        a.TransactionDate.Date == DateTime.Today.Date && a.Type == TransactionType.Income).ToList()
-                    .Sum(a => a.Amount);
-                homeViewModel.WeeklyExpenseAmount = transactionQueryable.Where(a =>
-                        a.TransactionDate.Date >= DateTime.Today.AddDays(-7).Date &&
-                        a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Expense).ToList()
-                    .Sum(a => a.Amount);
-                homeViewModel.WeeklyIncomeAmount = transactionQueryable.Where(a =>
-                        a.TransactionDate.Date >= DateTime.Today.AddDays(-7).Date &&
-                        a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Income).ToList()
-                    .Sum(a => a.Amount);
-                homeViewModel.MonthlyExpenseAmount = transactionQueryable.Where(a =>
-                        a.TransactionDate.Date >= DateTime.Today.AddMonths(-1).Date &&
-                        a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Expense).ToList()
-                    .Sum(a => a.Amount);
-                homeViewModel.MonthlyIncomeAmount = transactionQueryable.Where(a =>
-                        a.TransactionDate.Date >= DateTime.Today.AddMonths(-1).Date &&
-                        a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Income).ToList()
-                    .Sum(a => a.Amount);
-                    
-                return View(homeViewModel);
+                .GroupBy(a => new
+                {
+                    CategoryName = a.TransactionCategory.CategoryName, CategoryId = a.TransactionCategory.Id,
+                    a.TransactionCategory.Color
+                })
+                .Select(x => new TopCategory()
+                {
+                    CategoryName = x.Key.CategoryName,
+                    Amount = x.Sum(z => z.Amount),
+                    CategoryId = x.Key.CategoryId,
+                    Color = x.Key.Color
+                }).OrderByDescending(a => a.Amount).ToList();
+            homeViewModel.AllCategories = await _transactionCategoryRepository.GetAllAsync().ConfigureAwait(true);
+            homeViewModel.DailyExpenseAmount = transactionQueryable.Where(a =>
+                    a.TransactionDate.Date == DateTime.Today.Date && a.Type == TransactionType.Expense).ToList()
+                .Sum(a => a.Amount);
+            homeViewModel.DailyIncomeAmount = transactionQueryable.Where(a =>
+                    a.TransactionDate.Date == DateTime.Today.Date && a.Type == TransactionType.Income).ToList()
+                .Sum(a => a.Amount);
+            homeViewModel.WeeklyExpenseAmount = transactionQueryable.Where(a =>
+                    a.TransactionDate.Date >= DateTime.Today.AddDays(-7).Date &&
+                    a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Expense).ToList()
+                .Sum(a => a.Amount);
+            homeViewModel.WeeklyIncomeAmount = transactionQueryable.Where(a =>
+                    a.TransactionDate.Date >= DateTime.Today.AddDays(-7).Date &&
+                    a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Income).ToList()
+                .Sum(a => a.Amount);
+            homeViewModel.MonthlyExpenseAmount = transactionQueryable.Where(a =>
+                    a.TransactionDate.Date >= DateTime.Today.AddMonths(-1).Date &&
+                    a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Expense).ToList()
+                .Sum(a => a.Amount);
+            homeViewModel.MonthlyIncomeAmount = transactionQueryable.Where(a =>
+                    a.TransactionDate.Date >= DateTime.Today.AddMonths(-1).Date &&
+                    a.TransactionDate.Date <= DateTime.Today.Date && a.Type == TransactionType.Income).ToList()
+                .Sum(a => a.Amount);
+
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
