640afc757b9e5125bcbe4b12da2beaf3f3ca98a1
Kushal <niroulakushal31@gmail.com>
2021-11-10 12:50:43 +0545

    Feat: add App_Helper and initialze there

--- src/ExpenseTracker.Web/wwwroot/js/app.js
+++ src/ExpenseTracker.Web/wwwroot/js/app.js
@@ -1,1 +1,33 @@
-﻿
+﻿const APP_HELPER = {};
+
+APP_HELPER.Loaded = (callback) => {
+    document.addEventListener('DOMContentLoaded', callback);
+};
+
+APP_HELPER.InitializeSingleTypeahead = (elem) => {
+    new TomSelect(elem, {
+        allowEmptyOption: true,
+        placeholder: elem.dataset.placeholder ?? "Select an option",
+        render: {
+            option: function (data, escape) {
+                const icon = data.icon ? `<i class="${data.icon}"></i>  ` : '';
+                return '<div>' +
+                    '<span class="title">' + icon + escape(data.text) + '</span>' +
+                    '</div>';
+            },
+            item: function (data, escape) {
+                const icon = data.icon ? `<i class="${data.icon}"></i>  ` : '';
+                 return '<div>' +
+                    '<span class="title">' + icon + escape(data.text) + '</span>' +
+                    '</div>';
+            }
+        }
+    });
+};
+
+APP_HELPER.InitializeTypeahead = () => {
+    const elems = document.querySelectorAll('select:not(.ignore-typeahead');
+    for (const elem of elems) {
+        APP_HELPER.InitializeSingleTypeahead(elem);
+    }
+};
